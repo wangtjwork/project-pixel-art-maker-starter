0526f6e12c6ca8cf7ec6f46b6ddf57cbe4226d41
Tianjian Wang <wangtjwork@gmail.com>
2017-12-30 12:46:14 -0600

    Add Javascript for color picker

--- designs.js
+++ designs.js
@@ -1,5 +1,18 @@
 // Select color input
+let penColor = 'black';
+
+const colorPicker = document.querySelector('#colorPicker');
+colorPicker.addEventListener("change", choosePenColor, false);
+
+function choosePenColor (event) {
+  penColor = event.target.value;
+}
+
+
 // Select size input
+let height = 1, width = 1;
+
+
 
 // When size is submitted by the user, call makeGrid()
 
